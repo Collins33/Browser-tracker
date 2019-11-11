c40288403d7c053086ebf934307c27ac94735c21
collins njau <collinsnjau39@gmail.com>
2019-11-11 20:16:23 +0300

    Decode password

--- mpesa.py
+++ mpesa.py
@@ -1,6 +1,8 @@
-import requests
 import os
 from datetime import datetime
+import base64
+
+import requests
 
 phone_number = os.getenv('PHONE_NUMBER')
 business_short_code = os.getenv('BUSINESS_SHORT_CODE')
@@ -10,7 +12,12 @@ consumer_secret = os.getenv('CONSUMER_SECRET')
 
 unformated_time =  datetime.now()
 formatted_date = unformated_time.strftime("%Y%m%d%H%M%S")
-print(formatted_date)
+
+#business_short_code+pass_key+formatted_date
+date_to_encode = business_short_code+pass_key+formatted_date
+encoded_string = base64.b64encode(date_to_encode.encode()) # encode it into binary
+decoded_password = encoded_string.decode('utf-8')
+
 
 def lipa_na_mpesa():
 
@@ -19,7 +26,7 @@ def lipa_na_mpesa():
   headers = { "Authorization": "Bearer %s" % access_token }
   request = {
       "BusinessShortCode": business_short_code,
-      "Password": " ",
+      "Password": decoded_password,
       "Timestamp": formatted_date,
       "TransactionType": "CustomerPayBillOnline",
       "Amount": "5",
