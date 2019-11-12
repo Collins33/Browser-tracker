8d28c3e0f31a431c9090e4571ba8ec19d1b0022d
collins njau <collinsnjau39@gmail.com>
2019-11-12 12:36:43 +0300

    Generate credentials - generate token - make transaction - fix token issue

--- mpesa.py
+++ mpesa.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from requests.auth import HTTPBasicAuth
 import base64
 
 import requests
@@ -10,34 +11,44 @@ pass_key = os.getenv('PASS_KEY')
 consumer_key = os.getenv('CONSUMER_KEY')
 consumer_secret = os.getenv('CONSUMER_SECRET')
 
-unformated_time =  datetime.now()
+unformated_time = datetime.now()
 formatted_date = unformated_time.strftime("%Y%m%d%H%M%S")
 
-#business_short_code+pass_key+formatted_date
+business_short_code+pass_key+formatted_date
 date_to_encode = business_short_code+pass_key+formatted_date
-encoded_string = base64.b64encode(date_to_encode.encode()) # encode it into binary
+encoded_string = base64.b64encode(
+    date_to_encode.encode())  # encode it into binary
 decoded_password = encoded_string.decode('utf-8')
 
 
-def lipa_na_mpesa():
+def generate_credentials():
+    api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    auth_response = requests.get(
+        api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+    return auth_response.json()['access_token']
 
-  access_token = "Access-Token"
-  api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
-  headers = { "Authorization": "Bearer %s" % access_token }
-  request = {
-      "BusinessShortCode": business_short_code,
-      "Password": decoded_password,
-      "Timestamp": formatted_date,
-      "TransactionType": "CustomerPayBillOnline",
-      "Amount": "5",
-      "PartyA": phone_number,
-      "PartyB": " ",
-      "PhoneNumber": phone_number,
-      "CallBackURL": "https://muru35portfolio.herokuapp.com",
-      "AccountReference": "11223456",
-      "TransactionDesc": "Pay school fees"
-  }
-    
-  response = requests.post(api_url, json = request, headers=headers)
-    
-  print (response.text)
+
+def lipa_na_mpesa():
+    access_token = generate_credentials()
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = {"Authorization": "Bearer %s" % access_token}
+    request = {
+        "BusinessShortCode": business_short_code,
+        "Password": decoded_password,
+        "Timestamp": formatted_date,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": "1",
+        "PartyA": phone_number,
+        "PartyB": business_short_code,
+        "PhoneNumber": phone_number,
+        "CallBackURL": "https://muru35portfolio.herokuapp.com",
+        "AccountReference": "11223456",
+        "TransactionDesc": "Pay school fees"
+    }
+
+    response = requests.post(api_url, json=request, headers=headers)
+
+    print(response.text)
+
+
+lipa_na_mpesa()
