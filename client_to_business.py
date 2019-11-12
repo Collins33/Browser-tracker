5cddc60df6416f08e5d0986cc5d4b9c523938d81
collins njau <collinsnjau39@gmail.com>
2019-11-12 15:18:38 +0300

    Send request to register urls - create reusable function - change from http to https

--- client_to_business.py
+++ client_to_business.py
@@ -1,16 +1,40 @@
+import os
+
 import requests
 from generate_credentials import generate_credentials
 
 # REGISTER THE URLs FIRST
 
 access_token = generate_credentials()
-api_url = "http://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
-headers = {"Authorization": "Bearer %s" % access_token}
-request = {"ShortCode": " ",
-           "ResponseType": " ",
-           "ConfirmationURL": "http://ip_address:port/confirmation",
-           "ValidationURL": "http://ip_address:port/validation_url"}
+short_code = os.getenv('SHORT_CODE')
+msisdn_number = os.getenv('MSISDN')
 
-response = requests.post(api_url, json=request, headers=headers)
 
-print(response.text)
+def register_urls():
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
+    headers = {"Authorization": "Bearer %s" % access_token}
+    request = {"ShortCode": short_code,
+               "ResponseType": "Completed",
+               "ConfirmationURL": "https://muru35portfolio.herokuapp.com",
+               "ValidationURL": "https://muru35portfolio.herokuapp.com"}
+
+    response = requests.post(api_url, json=request, headers=headers)
+
+    print(response.text)
+
+
+def simulate_client_to_business_transaction():
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate"
+    headers = {"Authorization": "Bearer %s" % access_token}
+    request = {"ShortCode": short_code,
+               "CommandID": "CustomerPayBillOnline",
+               "Amount": "2",
+               "Msisdn": msisdn_number,
+               "BillRefNumber": "12345678"}
+
+    response = requests.post(api_url, json=request, headers=headers)
+
+    print(response.text)
+
+
+simulate_client_to_business_transaction()
