62226026732825a13264c23fce1782b991d46f77
collins njau <collinsnjau39@gmail.com>
2019-11-14 11:11:38 +0300

    Install dependencies

--- lipa_na_mpesa_online.py
+++ lipa_na_mpesa_online.py
@@ -8,9 +8,6 @@ from utils import format_date, encode_password_String
 phone_number = os.getenv('PHONE_NUMBER')
 business_short_code = os.getenv('BUSINESS_SHORT_CODE')
 pass_key = os.getenv('PASS_KEY')
-consumer_key = os.getenv('CONSUMER_KEY')
-consumer_secret = os.getenv('CONSUMER_SECRET')
-
 
 formatted_date = format_date()
 decoded_password = encode_password_String(
