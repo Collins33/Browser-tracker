20b7bc49459e030f5baebb6e9ef36f5eb6ac1bb4
collins njau <collinsnjau39@gmail.com>
2019-11-14 15:39:39 +0300

    Configure the local database

--- src/daraja/settings/__init__.py
+++ src/daraja/settings/__init__.py
@@ -1,6 +1,6 @@
-from .production_settings import *
+# from .production_settings import *
 
-# try:
-#     from .local_settings import *
-# except:
-#     pass
+try:
+    from .local_settings import *
+except:
+    pass
--- src/daraja/settings/local_settings.py
+++ src/daraja/settings/local_settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'daraja.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_OWNER_NAME'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
